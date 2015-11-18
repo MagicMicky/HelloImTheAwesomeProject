7e56ed4260047ac0660a9b097c85a19a379ddd25
Mickael Goubin <micky0304@gmail.com>
2015-11-18 22:48:19 +0100

    Failing test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
 
     @org.junit.Test
     public void testAdd() throws Exception {
-        assertEquals(Main.add(0,2), 2);
+        assertEquals(Main.add(0,3), 2);
         assertEquals(Main.add(-1,1), 0);
         assertEquals(Main.add(9999,1), 10000);
     }
