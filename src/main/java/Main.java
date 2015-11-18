29ef429b73081918aa203a39189b3bf981558c6f
Mickael Goubin <micky0304@gmail.com>
2015-11-18 21:47:18 +0100

    Init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by magicmicky on 18/11/15.
+ */
+public class Main {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        add(1, 2);
+    }
+
+}
