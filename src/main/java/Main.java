64d40fa56deb574d1135bd6649c318028c35935d
Mickael Goubin <micky0304@gmail.com>
2015-11-18 22:43:33 +0100

    Here comes the tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,7 @@ public class Main {
     public static int add(int a, int b) {
         return a+b;
     }
-
     public static void main(String[] args) {
         add(1, 2);
     }
-
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by magicmicky on 18/11/15.
+ */
+public class MainTest {
+
+    @org.junit.Test
+    public void testAdd() throws Exception {
+        assertEquals(Main.add(0,2), 2);
+        assertEquals(Main.add(-1,1), 0);
+        assertEquals(Main.add(9999,1), 10000);
+    }
+}
